881872520cd97c70cb461bd3e99fac0d58637a11
Erick Saavedra <esvdrah@gmail.com>
2020-07-21 18:25:21 -0500

    styled components for the main content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import styled from '@emotion/styled';
+import imagen from './cryptomonedas.png';
+
+const Container = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 100%;
+  margin-top: 5rem;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Bebas Neue', cursive;
+  color: #fff;
+  text-align: left;
+  font-weight: 700;
+  font-size: 50px;
+  margin-top: 80px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+  }
+`;
 
 function App() {
-  return <h1>Cryptocurrency Trader</h1>;
+  return (
+    <Container>
+      <div>
+        <Imagen src={imagen} alt='image crypto' />
+      </div>
+      <div>
+        <Heading>Instant Results</Heading>
+      </div>
+    </Container>
+  );
 }
 
 export default App;
